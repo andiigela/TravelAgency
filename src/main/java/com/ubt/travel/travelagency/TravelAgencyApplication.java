1b4a7d4a6e7a958ba35fa3189bfb028d6b7b1910
andiigela <109698122+andiigela@users.noreply.github.com>
2023-10-21 15:15:14 +0200

    main code

---
+++ src/main/java/com/ubt/travel/travelagency/TravelAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.ubt.travel.travelagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAgencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ubt/travel/travelagency/TravelAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubt.travel.travelagency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
